44512d65a438ddf0006ad44f1d802ef30bf22130
leobuss <buss14@purdue.edu>
2025-11-19 18:48:19 -0500

    Update clockDivider.sv
    changed it to make it a single tick high instead of half of the pulse

--- clockDivider.sv
+++ clockDivider.sv
@@ -1,29 +1,28 @@
 
-module clk_div_12000(
-    input  logic clk,        // input clock
-    input  logic reset,      // ACTIVE-HIGH reset
-    output logic clk_out     // divided clock output
+module clk_div_100(
+    input  logic clk, // input clock
+    input  logic reset, // ACTIVE-HIGH reset
+    output logic clk_out // divided clock output
 );
 
     
-    localparam int HALF_COUNT = 6000;
-
-    
-    logic [12:0] count;
+    logic [6:0] count;
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            count   <= 0;
-            clk_out <= 0;
+            count   <= 7'd0;
+            clk_out <= 1'b0;
         end
         else begin
-            if (count == HALF_COUNT - 1) begin
-                count   <= 0;
-                clk_out <= ~clk_out;  
+            if (count == 7'd99) begin
+                count   <= 7'd0;
+                clk_out <= 1'b1;
             end
             else begin
-                count <= count + 1;
+                count   <= count + 7'd1;
+                clk_out <= 1'b0;
             end
         end
     end
     endmodule
+

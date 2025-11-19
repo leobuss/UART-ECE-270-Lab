2dde75cd9a4fc791a1920f41710aea4d8bad46a3
leobuss <buss14@purdue.edu>
2025-11-19 18:50:20 -0500

    Update clockDivider.sv
    made it run on its own no reset needed

--- clockDivider.sv
+++ clockDivider.sv
@@ -1,28 +1,25 @@
 
 module clk_div_100(
     input  logic clk, // input clock
-    input  logic reset, // ACTIVE-HIGH reset
     output logic clk_out // divided clock output
 );
-
     
     logic [6:0] count;
+    initial begin
+        count   = 7'd0;
+        clk_out = 1'b0;
+    end
 
-    always_ff @(posedge clk or posedge reset) begin
-        if (reset) begin
+    always_ff @(posedge clk) begin
+        if (count == 7'd99) begin
             count   <= 7'd0;
-            clk_out <= 1'b0;
+            clk_out <= 1'b1;
         end
         else begin
-            if (count == 7'd99) begin
-                count   <= 7'd0;
-                clk_out <= 1'b1;
-            end
-            else begin
-                count   <= count + 7'd1;
-                clk_out <= 1'b0;
-            end
+            count   <= count + 7'd1;
+            clk_out <= 1'b0;
         end
     end
     endmodule
+
 

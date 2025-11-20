c2d0b857726ee391e2ccc34284dfcda4578b0fde
leobuss <buss14@purdue.edu>
2025-11-19 21:37:09 -0500

    Update timer.sv
    with milliseconds

--- timer.sv
+++ timer.sv
@@ -1,7 +1,6 @@
 module timer(
     input logic clk, //clock
     input logic reset, //reset active high
-    input logic en, // enable (divided clock)
     input logic inc, dec, // either increase or decrease
     input logic state, // button to increment mode or clear expired time
     input logic start, // button to start/stop
@@ -9,6 +8,7 @@ module timer(
     output logic [5:0] hours, // hours place
     output logic [5:0] minutes, // minutes plalce
     output logic [5:0] seconds, // seconds place
+    output logic [6:0] milliseconds, // decimal places
     output logic done // 1 when timer has expired
 );
 
@@ -19,6 +19,7 @@ module timer(
     run_t run_state, next_run_state;
 
     logic [5:0] next_hours, next_minutes, next_seconds;
+    logic [6:0] next_milliseconds;
     logic next_done;
     
     //flip flop
@@ -27,6 +28,7 @@ module timer(
             hours <= 6'd0;
             minutes <= 6'd0;
             seconds <= 6'd0;
+            milliseconds <= 7'd0;
             mode <= HOURS;   
             run_state <= STOPPED;
             done <= 1'b0;
@@ -35,6 +37,7 @@ module timer(
             hours <= next_hours;
             minutes <= next_minutes;
             seconds <= next_seconds;
+            milliseconds <= next_milliseconds;
             mode <= next_mode;
             run_state <= next_run_state;
             done <= next_done;
@@ -46,6 +49,7 @@ module timer(
         next_hours = hours;
         next_minutes = minutes;
         next_seconds = seconds;
+        next_milliseconds = milliseconds;
         next_mode = mode;
         next_run_state = run_state;
         next_done = done;
@@ -114,33 +118,40 @@ module timer(
                 end
             end
         end
-        if (mode == CLOCK && run_state == RUNNING && en && sel == 2'd2) begin
+         if (mode == CLOCK && run_state == RUNNING && sel == 2'd2) begin
             // already at zero
-            if (hours == 6'd0 && minutes == 6'd0 && seconds == 6'd0) begin
+            if (hours == 6'd0 && minutes == 6'd0 && seconds == 6'd0 && milliseconds == 7'd0) begin
                 next_run_state = EXPIRED;
-                next_done = 1'b1;
+                next_done      = 1'b1;
             end
             else begin
-                // decrement
-                if (seconds == 6'd0) begin
-                    next_seconds = 6'd59;
-                    if (minutes == 6'd0) begin
-                        next_minutes = 6'd59;
-                        if (hours == 6'd0)
-                            next_hours = 6'd0; //unreachable
+                // decrement 0.01s
+                if (milliseconds == 7'd0) begin
+                    next_milliseconds = 7'd99;
+                    if (seconds == 6'd0) begin
+                        next_seconds = 6'd59;
+                        if (minutes == 6'd0) begin
+                            next_minutes = 6'd59;
+                            if (hours == 6'd0)
+                                next_hours = 6'd0;
+                            else
+                                next_hours = hours - 1;
+                        end
                         else
-                            next_hours = hours - 1;
+                            next_minutes = minutes - 1;
                     end
                     else
-                        next_minutes = minutes - 1;
+                        next_seconds = seconds - 1;
                 end
-                else
-                    next_seconds = seconds - 1;
+                else begin
+                    next_milliseconds = milliseconds - 1;
+                end
                 
                 // if next value is 0 go to expired
-                if (next_hours == 6'd0 && next_minutes == 6'd0 && next_seconds == 6'd0) begin
+                if (next_hours == 6'd0 && next_minutes == 6'd0 &&
+                    next_seconds == 6'd0 && next_milliseconds == 7'd0) begin
                     next_run_state = EXPIRED;
-                    next_done = 1'b1;
+                    next_done      = 1'b1;
                 end
             end
         end

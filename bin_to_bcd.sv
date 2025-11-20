ab20f6af700cf767edc52b28e1748df7b96d8e33
Amogh Shivanand Havanagi <ahavanag@ecelnx26.ecn.purdue.edu>
2025-11-20 18:36:02 -0500

    agartha

--- bin_to_bcd.sv
+++ bin_to_bcd.sv
@@ -1,20 +1,19 @@
 `default_nettype none
 
 module bin_to_bcd(
-    input  logic [6:0] num, // 0 to 99
-    output logic [3:0] tens, // tens digit 0–9
-    output logic [3:0] ones // ones digit 0–9
+    input  logic [6:0] num, 
+    output logic [3:0] tens, // tens digit 
+    output logic [3:0] ones // ones digit 
 );
 
     always_comb begin
         integer tmp;
         integer t;
 
-        tmp = num;   // copy of input
+        tmp = num; 
         t   = 0;
 
-        // subtract 10 as many times as possible, up to 9 times
-        // no division or modulus, fully synthesizable
+  
         for (int i = 0; i < 10; i++) begin
             if (tmp >= 10) begin
                 tmp = tmp - 10;

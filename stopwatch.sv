da3d3a51e69d7d4420a33e650847968e65ecb6c8
leobuss <buss14@purdue.edu>
2025-11-19 22:01:01 -0500

    Update stopwatch.sv
    updated

--- stopwatch.sv
+++ stopwatch.sv
@@ -2,12 +2,12 @@ module stopwatch(
 
     input logic clk, //clock
     input logic reset, //reset active high
-    input logic en, // enable (divided clock)
     input logic start, // button to start/stop
-    input logic [2:0] sel,
+    input logic [1:0] sel,
     output logic [5:0] hours, // hours place
     output logic [5:0] minutes, // minutes plalce
-    output logic [5:0] seconds // seconds place
+    output logic [5:0] seconds, // seconds place
+    output logic [6:0] milliseconds // milliseconds
     
 
 );
@@ -15,6 +15,7 @@ module stopwatch(
 
     typedef enum logic [1:0] {STOPPED, RUNNING} mode_t;
     logic [5:0] next_hours, next_minutes, next_seconds;
+    logic [6:0] next_milliseconds;
     mode_t mode, next_mode;
     
     
@@ -23,12 +24,14 @@ module stopwatch(
             hours <= 6'd0;
             minutes <= 6'd0;
             seconds <= 6'd0;
+            milliseconds <= 7'd0;
             mode <= STOPPED;
         end
         else begin
             hours <= next_hours;
             minutes <= next_minutes;
             seconds <= next_seconds;
+            milliseconds <= next_milliseconds;
             mode <= next_mode;
         end
     end   
@@ -37,6 +40,7 @@ module stopwatch(
         next_hours = hours;
         next_minutes = minutes;
         next_seconds = seconds;
+        next_milliseconds = milliseconds;
         next_mode = mode;
         
         if (start && sel != 2'd2) begin
@@ -48,26 +52,32 @@ module stopwatch(
         end
         
         if (mode == RUNNING) begin
-            if (en) begin
-                if (hours == 6'd23 && minutes == 6'd59 && seconds == 6'd59) begin
-                    // freeze at max time
-                end
-                else if (seconds == 6'd59) begin
+            // freeze at max
+            if (hours == 6'd23 && minutes == 6'd59 &&
+                seconds == 6'd59 && milliseconds == 7'd99) begin
+                // do nothing
+            end
+            else if (milliseconds == 7'd99) begin
+                next_milliseconds = 7'd0;
+                if (seconds == 6'd59) begin
                     next_seconds = 6'd0;
                     if (minutes == 6'd59) begin
                         next_minutes = 6'd0;
                         if (hours == 6'd23)
                             next_hours = 6'd23; // unreachable because of freeze check
                         else
-                            next_hours = hours + 1;
+                            next_hours = hours + 6'd1;
                     end
                     else
-                        next_minutes = minutes + 1;
+                        next_minutes = minutes + 6'd1;
                 end
-                else
-                    next_seconds = seconds + 1;
+                else begin
+                    next_seconds = seconds + 6'd1;
+                end
+            end
+            else begin
+                next_milliseconds = milliseconds + 7'd1;
             end
         end
     end
 endmodule
-

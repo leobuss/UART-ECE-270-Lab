a66deef40f6889098a66d87d203621d016c95c6c
leobuss <buss14@purdue.edu>
2025-11-21 13:37:00 -0500

    Create pb_divider.sv

---
+++ pb_divider.sv
@@ -0,0 +1,28 @@
+
+module pb_divider (
+    input logic clk,
+    input logic in,
+    output logic out
+);
+    
+  logic [4:0] count;
+  initial begin
+      count   = 5'd0;
+      out = 1'b0;
+  end
+
+    
+    always_ff @(posedge clk) begin
+      if (count == 5'd24) begin
+            count   <= 5'd0;
+            out <= in;
+        end
+        else begin
+            count   <= count + 5'd1;
+            out <= 1'b0;
+        end
+    end
+endmodule
+
+
+

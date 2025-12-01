ed47aa09fd80f66a7fefc83efa2ecdffed85e2ca
leobuss <buss14@purdue.edu>
2025-12-01 10:28:23 -0500

    Update pb_divider.sv

--- pb_divider.sv
+++ pb_divider.sv
@@ -7,18 +7,18 @@ module pb_divider (
     
   logic [4:0] count;
   initial begin
-      count   = 5'd0;
+      count = 5'd0;
       out = 1'b0;
   end
 
     
     always_ff @(posedge clk) begin
       if (count == 5'd24) begin
-            count   <= 5'd0;
+            count <= 5'd0;
             out <= in;
         end
         else begin
-            count   <= count + 5'd1;
+            count <= count + 5'd1;
             out <= 1'b0;
         end
     end

71d78691fcb159dcf8252d229e7bdffe3cc30dd3
leobuss <buss14@purdue.edu>
2025-12-01 10:29:07 -0500

    Update selectLatch.sv

--- selectLatch.sv
+++ selectLatch.sv
@@ -1,25 +1,33 @@
+// depending on input, change the current display signal (if no input, remain same)
 module selectLatch(
-    input logic [1:0] sel, //pb 1:0
+    input logic [2:0] sel, //buttons 1:0
     input logic clk, rst,
     output logic [1:0] out //out
 );
 
-logic [1:0] n_out;
+typedef enum logic [1:0] {STOPWATCH = 2'b11, TIMER=2'd2, CLOCK=2'd1} state_t;
+state_t state, next_state;
+
 
 always_ff @(posedge clk, posedge rst) begin
     if(rst) begin
-        out => 2'd1;
+        state <= CLOCK;
     end else begin
-        out => n_out;
+        state <= next_state;
     end
 end 
 
 always_comb begin
-    if (sel = 2'd0) begin
-        n_out = out;
-    end else begin
-        n_out = sel;
+    next_state = state;
+    if (sel == 3'd1) begin
+        next_state = CLOCK;
+    end else if(sel == 3'd2) begin
+        next_state = TIMER;
+    end else if(sel == 3'd4) begin
+        next_state = STOPWATCH;
     end
+
+    out = state;
 end
 
 

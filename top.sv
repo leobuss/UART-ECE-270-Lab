8de29bc26ec8e97697a9b457cf4a22ce3f208509
leobuss <buss14@purdue.edu>
2025-12-01 10:28:40 -0500

    comments added

--- top.sv
+++ top.sv
@@ -268,11 +268,11 @@ module pb_divider (
     
     always_ff @(posedge clk) begin
       if (count == 5'd24) begin
-            count   <= 5'd0;
+            count <= 5'd0;
             out <= in;
         end
         else begin
-            count   <= count + 5'd1;
+            count <= count + 5'd1;
             out <= 1'b0;
         end
     end
